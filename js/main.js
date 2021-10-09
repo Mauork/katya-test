3426d4da6d69bfb407427795b10baf9d4892a2cf
Beletskyi-Moawad Martin <m.beleckii@alef.im>
2021-10-09 13:20:12 +0300

    Добавлена логика для избранного

--- js/main.js
+++ js/main.js
@@ -12,3 +12,27 @@ go_up_btn.addEventListener("click", function () {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 });
+
+function favoriteLogic() {
+  var favorites = document.querySelectorAll(".card__favorite");
+
+  [].forEach.call(favorites, function (element, index) {
+    element.addEventListener("click", function () {
+      if (element.className.indexOf("selected") === -1) {
+        alert("Кот добавлен в избранное!");
+        element.className += " card__favorite-selected";
+        return;
+      }
+
+      alert("Кот удален из избранных!");
+      element.className = element.className
+        .split(" ")
+        .filter(function (el_class) {
+          return el_class !== "card__favorite-selected";
+        })
+        .join("");
+    });
+  });
+}
+
+favoriteLogic();
